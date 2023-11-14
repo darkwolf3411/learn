36ee5a5047c0bf8354898931e0f681bc8c937f81
Kirill <40362101+darkwolf3411@users.noreply.github.com>
2023-11-14 19:54:47 +0600

    webpack less css modules

--- WebpackLearn/src/components/App.tsx
+++ WebpackLearn/src/components/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react"
-import './App.scss'
+import classes from './App.module.scss'
 
 export const App = () => {
     const [count, setCount] = useState(0);
     const inctement = () => setCount(prev => prev + 1)
   return (
     <div>
-    <div>{count}</div>
-    <button onClick={inctement}>Click</button>
+    <div className={classes.item}>{count}</div>
+    <button className={classes.button} onClick={inctement}>Click</button>
     </div>
   )
 }
